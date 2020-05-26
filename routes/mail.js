716d351db0c0ad180787686720e54abe1f488b4a
Prakash <pk4501020@gmail.com>
2020-05-26 14:30:22 +0530

    cross origin bug fixed

--- routes/mail.js
+++ routes/mail.js
@@ -22,14 +22,14 @@ router.post('/send',(req,res)=>{
         }
         transporter.sendMail(mailOptions,(err,data)=>{
             if(err){
-                res.setHeader('Access-Control-Allow-Origin', 'https://filefly-send.herokuapp.com/');
+                res.setHeader('Access-Control-Allow-Origin', 'https://filefly-download.herokuapp.com/');
                 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
                 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
                 res.setHeader('Access-Control-Allow-Credentials', true);
                 res.status(500)
                 res.json({msg:'Internal server error'})
             }else{
-                res.setHeader('Access-Control-Allow-Origin', 'https://filefly-send.herokuapp.com/');
+                res.setHeader('Access-Control-Allow-Origin', 'https://filefly-download.herokuapp.com/');
                 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
                 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
                 res.setHeader('Access-Control-Allow-Credentials', true);
@@ -39,7 +39,7 @@ router.post('/send',(req,res)=>{
         })
     }
     catch(err){
-        res.setHeader('Access-Control-Allow-Origin', 'https://filefly-send.herokuapp.com/');
+        res.setHeader('Access-Control-Allow-Origin', 'https://filefly-download.herokuapp.com/');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
