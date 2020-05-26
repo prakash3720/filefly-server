7ffa35929030a82c7e4849b89bea067e6f970af2
Prakash <pk4501020@gmail.com>
2020-05-26 15:39:19 +0530

    cors module

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(cors())
 
 app.use(deleteMiddleware)
 app.use('/upload',upload)
